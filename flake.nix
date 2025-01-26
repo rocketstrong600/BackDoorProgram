ece2e0e1bde3609f3216a072fce4fedbe680f16a
Ethan Johnston <rocketstrong600@gmail.com>
2025-01-27 04:13:03 +1000

    flake stuff

--- flake.nix
+++ flake.nix
@@ -8,10 +8,10 @@
     utils.lib.eachDefaultSystem(system:
       let
         pkgs = import nixpkgs { inherit system; };
-        python-with-pip = pkgs.python3.withPackages (p: with p; [pip]);
+        #python-with-pip = pkgs.python3.withPackages (p: with p; [pip]);
         in {
           devShell = with pkgs; mkShell {
-            buildInputs = [ platformio esptool python-with-pip]; 
+            buildInputs = [ platformio esptool pip]; 
             shellHook = ''
               export PLATFORMIO_CORE_DIR=$PWD/.platformio
             '';
